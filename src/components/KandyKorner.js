6b8081987854f77465f61761c0b7aa157ef3d9c1
Jameka Echols <jameka.echols@gmail.com>
2019-04-11 18:15:11 -0500

    added arrays

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class KandyKorner extends Component {
+    
+    //Store locations array
+    storeLocations = [
+        { id:1, address: "123 Lean On Me Drive, Nashville, TN" },
+        { id:2, address: "872 Rock With It, Clarksville, TN" },
+        { id:3, address: "9019 Hottie Ave,  Bristol, TN" },
+    ]
+
+    //Employees array
+    employees = [
+        { id:1, name: "Jameka Echols" },
+        { id:2, name: "DeSean Webster" }
+    ]
+    
+    
+    state = {
+
+    }
+}
