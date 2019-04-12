18739bf3a6dd02aa401e9dd605059e463ec41d12
Jameka Echols <jameka.echols@gmail.com>
2019-04-12 13:22:06 -0500

    added bootstrap

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import NavBar from './nav/NavBar'
 import ApplicationView from './../ApplicationView';
-// import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css"
 
 export default class KandyKorner extends Component {
 
